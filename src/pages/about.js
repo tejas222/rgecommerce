ed2771b2d4a22d1e9331456d3cf5211b88b7cf0b
tejas <tejasj15@gmail.com>
2021-07-22 21:19:29 +0530

    about updated

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,7 +7,7 @@ import Layout from "../components/layout"
 import Seo from "../components/seo"
 import HeroSection from "../components/Reusable/HeroSection"
 import Infoblock from "../components/Reusable/Infoblock" 
-import DualInfoblock from "../components/Reusable/Dualinfoblock"
+import DualInfoblock from "../components/Reusable/DualInfoblock"
 import Teamsection from "../components/About/Teamsection"
 
 
