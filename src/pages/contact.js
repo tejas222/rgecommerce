4cf014f1e805bc808c3a1a0935bce4f9b6fb9243
tejas <tejasj15@gmail.com>
2021-07-22 21:01:38 +0530

    contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,42 +1,10 @@
-import * as React from "react"
-import { graphql, useStaticQuery } from 'gatsby'
-import { StaticImage } from "gatsby-plugin-image"
+import React from 'react'
 
-import "../components/bootstrap.min.css"
-import Layout from "../components/layout"
-import Seo from "../components/seo"
-import HeroSection from "../components/Reusable/HeroSection"
-import Infoblock from "../components/Reusable/Infoblock" 
-import DualInfoblock from "../components/Reusable/Dualinfoblock"
-import Teamsection from "../components/About/Teamsection"
-
-
-const AboutPage = ( {data} ) => (
-  <Layout>
-    <Seo title="About" />
-    <HeroSection
-      img = {data.img.childImageSharp.fluid}
-      title="About Me"
-      subtitle= ""
-      heroclass="about-background"
-     />
-      <DualInfoblock heading="Message From CEO"/>
-      <Infoblock heading="About Our Vision" />
-      <Teamsection />
-  </Layout>
-)
-
-export const query = graphql`
-{
-  img: file(relativePath: { eq: "about.png" }) {
-    childImageSharp {
-      fluid {
-        ...GatsbyImageSharpFluid_tracedSVG
-      }
-    }
-  }
+export default function 
+() {
+    return (
+        <div>
+            <h1>Contact page</h1>
+        </div>
+    )
 }
-
-`
-
-export default AboutPage
